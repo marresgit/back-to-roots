c01abf75085a347fe1d9602b6737135fb3b78ebb
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-05-19 22:30:19 +0200

    Working

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ API_KEY="378e3722-bb85-4e3c-9599-ecf118cf25ab"
 @app.route("/")
 def hello():
     r = requests.get(f'https://api.opendota.com/api/matches/271145478?api_key=378e3722-bb85-4e3c-9599-ecf118cf25ab')
-    print(r.status.code)
+    #print(r.status.code)
     return "Hello world"
 
 if __name__ == '__main__':
