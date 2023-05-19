22c53cbc43ae2360b97e7b0eadd1418b5dd4258e
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-05-19 22:32:13 +0200

    Ok, needed to convert to string :P

--- webapp.py
+++ webapp.py
@@ -7,9 +7,9 @@ API_KEY="378e3722-bb85-4e3c-9599-ecf118cf25ab"
 
 @app.route("/")
 def hello():
-    r = requests.get(f'https://api.opendota.com/api/matches/271145478?api_key=378e3722-bb85-4e3c-9599-ecf118cf25ab')
-    #print(r.status.code)
-    return "Hello world"
+    r = requests.get('https://api.opendota.com/api/matches/271145478?api_key=378e3722-bb85-4e3c-9599-ecf118cf25ab')
+
+    return "Hello world " + str(r.status_code)
 
 if __name__ == '__main__':
     app.run()
