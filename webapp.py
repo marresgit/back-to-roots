f2794ed0d40b9318ab3ed0e7dda494113cfa97e4
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-05-19 22:29:56 +0200

    1

--- webapp.py
+++ webapp.py
@@ -1,12 +1,15 @@
 from flask import Flask
+import requests
 
 app = Flask (__name__)
 
-
+API_KEY="378e3722-bb85-4e3c-9599-ecf118cf25ab"
 
 @app.route("/")
 def hello():
-    return "Hello again, flask"
+    r = requests.get(f'https://api.opendota.com/api/matches/271145478?api_key=378e3722-bb85-4e3c-9599-ecf118cf25ab')
+    print(r.status.code)
+    return "Hello world"
 
 if __name__ == '__main__':
     app.run()
