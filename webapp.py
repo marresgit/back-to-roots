d24b2387758d9ab97b0f60c10431391b61ba245a
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-05-23 22:50:22 +0200

    REAME.md

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ app = Flask (__name__)
 def hello():
     r = requests.get(f'https://api.opendota.com/api/players/62297546/matches?api_key={config.API_KEY}')
 
-    return "Hello world " + str(r.status_code) + " " + str(r.json())
+    return "Hello world " + str(r.status_code) + " " + str(r.json()) #try to print 10 games + what hero
 
 if __name__ == '__main__':
     app.run()
