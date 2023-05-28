5ae9db3db089f729c63f625d362370c109746749
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-05-28 22:50:19 +0200

    something happend

--- webapp.py
+++ webapp.py
@@ -1,14 +1,26 @@
 from flask import Flask
 import requests
 import config
+import json
 
 app = Flask (__name__)
 
 @app.route("/")
-def hello():
-    r = requests.get(f'https://api.opendota.com/api/players/62297546/matches?api_key={config.API_KEY}')
+def index():
+    rSteveAccount = requests.get(f'https://api.opendota.com/api/players/62297546/matches?api_key={config.API_KEY}')
+    # return r.json()
+    # hero_id =
 
-    return "Hello world " + str(r.status_code) + " " + str(r.json()) #TODO: print 10 games + hero
+    for request in rSteveAccount.json():
+        return request
+
+
+@app.route("/heroes")
+def heroes():
+    rDotaHeroes = requests.get(f'https://api.opendota.com/api/heroes?api_key={config.API_KEY}')
+    return rDotaHeroes.json()
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
